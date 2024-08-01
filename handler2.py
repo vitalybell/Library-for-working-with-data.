ab8846208ab3602478c92e88a44387a43407ef4f
vitalybell <163574647+vitalybell@users.noreply.github.com>
2024-08-01 14:39:43 +0300

    Add files via upload

---
+++ handler2.py
@@ -0,0 +1,22 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
+api = ''
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+@dp.message_handler(commands='start')
+async def start_messege(message):
+    print('У вас сообщение')
+    await message.answer('Привет! Я бот помогающий твоему здоровью.')
+
+
+@dp.message_handler()
+async def all_messege(message):
+    print('Новое сообщение')
+    await message.answer('Введите команду /start чтобы начать обшение')
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
