7d464892f2484f5232c78c94889eb029b3185470
vitalybell <163574647+vitalybell@users.noreply.github.com>
2024-08-06 21:35:21 +0300

    Add files via upload

---
+++ Keypad_buttons.py
@@ -0,0 +1,75 @@
+from aiogram import Bot, Dispatcher, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.utils import executor
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+
+api = ''
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+kb = ReplyKeyboardMarkup()
+button2 = KeyboardButton(text='Рассчитать')
+button = KeyboardButton(text='Информация')
+kb.add(button)
+kb.add(button2)
+
+
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+
+
+@dp.message_handler(commands="start")
+async def welcome(message: types.Message):
+    await message.answer("Привет!", reply_markup=kb)
+
+
+@dp.message_handler(text='Информация')
+async def welcome(message):
+    await message.answer("Привет!Я бот помогающий твоему здоровью. Напишите 'Рассчитать', "
+                         "чтобы начать.")
+
+
+@dp.message_handler(text='Рассчитать')
+async def set_age(message: types.Message):
+    await UserState.age.set()  # Sets the state to UserState.age
+    await message.answer("Введите свой возраст:")
+
+
+@dp.message_handler(state=UserState.age)
+async def set_growth(message: types.Message, state: FSMContext):
+    await state.update_data(age=int(message.text))  # Update age in state data
+    await UserState.growth.set()  # Set the next state
+    await message.answer("Введите свой рост (в см):")
+
+
+@dp.message_handler(state=UserState.growth)
+async def set_weight(message: types.Message, state: FSMContext):
+    await state.update_data(growth=int(message.text))  # Update growth in state data
+    await UserState.weight.set()  # Set the next state
+    await message.answer("Введите свой вес (в кг):")
+
+
+@dp.message_handler(state=UserState.weight)
+async def send_calories(message: types.Message, state: FSMContext):
+    await state.update_data(weight=int(message.text))  # Update weight in state data
+    data = await state.get_data()  # Get all stored data
+    age = data['age']
+    growth = data['growth']
+    weight = data['weight']
+
+    # Формула для расчета нормы калорий (Мифлин — Сан Жеор)
+    # Пример для женщин:
+    # BMR = 10 * weight + 6.25 * growth - 5 * age + 5
+    # Если у вас нужный тип пола - измените коэффициенты в формуле
+    bmr = 10 * weight + 6.25 * growth - 5 * age + 5
+    await message.answer(f"Ваша норма калорий: {bmr:.2f} ккал в день.")
+    await state.finish()  # Finish the state machine
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
